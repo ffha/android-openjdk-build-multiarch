19b7706589132165c0c05b64fa3f796fbe15e38a
Alan Feng <fthasdd@090124.xyz>
2023-08-05 14:59:09 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -10,6 +10,7 @@ if [ "$BUILD_IOS" != "1" ]; then
 
   wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
   ./extractndk.sh
+  ./maketoolchain.sh
 else
   chmod +x ios-arm64-clang
   chmod +x ios-arm64-clang++
