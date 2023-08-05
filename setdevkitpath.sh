579247917dc1ac1211e4212987157291cfe4a6e5
Alan Feng <fthasdd@090124.xyz>
2023-08-05 14:55:20 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -1,6 +1,6 @@
 # Use the old NDK r10e to not get internal compile error at (still?)
 # https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u/blob/aarch64-shenandoah-jdk8u272-b10/jdk/src/share/native/sun/java2d/loops/GraphicsPrimitiveMgr.c
-export NDK_VERSION=r21
+export NDK_VERSION=r25c
 
 if [ -z "$BUILD_FREETYPE_VERSION" ]
 then
@@ -48,8 +48,8 @@ export JVM_PLATFORM=linux
 export API=21
 export NDK=$PWD/android-ndk-$NDK_VERSION
 export ANDROID_NDK_ROOT=$NDK
-export TOOLCHAIN=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
-# export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+# export TOOLCHAIN=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
 
 export ANDROID_INCLUDE=$TOOLCHAIN/sysroot/usr/include
 
@@ -60,12 +60,12 @@ export thecc=$TOOLCHAIN/bin/$TARGET-gcc
 export thecxx=$TOOLCHAIN/bin/$TARGET-g++
 
 # Configure and build.
-export AR=$TOOLCHAIN/bin/$TARGET-ar
-export AS=$TOOLCHAIN/bin/$TARGET-as
-export CC=$PWD/android-wrapped-clang
-export CXX=$PWD/android-wrapped-clang++
-export LD=$TOOLCHAIN/bin/$TARGET-ld
-export OBJCOPY=$TOOLCHAIN/bin/$TARGET-objcopy
-export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
-export STRIP=$TOOLCHAIN/bin/$TARGET-strip
+export AR=$TOOLCHAIN/bin/llvm-ar
+export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+export AS=$CC
+export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+export LD=$TOOLCHAIN/bin/ld
+export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export STRIP=$TOOLCHAIN/bin/llvm-strip
+export OBJCOPY=$TOOLCHAIN/bin/llvm-objcopy
 fi
